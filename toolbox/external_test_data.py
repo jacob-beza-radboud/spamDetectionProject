eecc668da45eef8cbef8c2308b4d889a5c3cf11e
jakeISAC <jakesykulski@yahoo.com>
2023-01-07 18:57:10 +0100

    code part of the project - done :)

--- toolbox/external_test_data.py
+++ toolbox/external_test_data.py
@@ -1,14 +1,14 @@
 from pandas import read_csv
 
 def load_external_test_data():
-    load = read_csv("./data/test_spam_model.csv")
+    load = read_csv("./data/spam_database.csv")
     load_arr = []
     converted = []
     test_label = []
 
     for index, row in load.iterrows():
-        load_arr.append(row['Message'])
-        if row['Category'] == "spam":
+        load_arr.append(row['text'])
+        if row['type'] == "spam":
             test_label.append(1)
         else:
             test_label.append(0)
